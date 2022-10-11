62dd51fc5629cdfbee9b7e93e924739a714f01ca
aleafarm <rafaelademellomartins879@gmail.com>
2022-10-10 23:02:29 -0300

    Primeiros ajustes aplicados!

--- Biblioteca/Controllers/EmprestimoController.cs
+++ Biblioteca/Controllers/EmprestimoController.cs
@@ -11,6 +11,8 @@ namespace Biblioteca.Controllers
     {
         public IActionResult Cadastro()
         {
+            Autenticacao.CheckLogin(this);
+
             LivroService livroService = new LivroService();
             EmprestimoService emprestimoService = new EmprestimoService();
 
@@ -22,6 +24,8 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
         {
+
+
             EmprestimoService emprestimoService = new EmprestimoService();
             
             if(viewModel.Emprestimo.Id == 0)
@@ -35,8 +39,11 @@ namespace Biblioteca.Controllers
             return RedirectToAction("Listagem");
         }
 
-        public IActionResult Listagem(string tipoFiltro, string filtro)
+        public IActionResult Listagem(string tipoFiltro, string filtro )
         {
+
+            Autenticacao.CheckLogin(this);
+            
             FiltrosEmprestimos objFiltro = null;
             if(!string.IsNullOrEmpty(filtro))
             {
